1a9f0b6b9eb7a8b1c54d13a8bd274d518b221fa5
mraksoll4 <32709596+mraksoll4@users.noreply.github.com>
2026-06-11 23:37:27 +0300

    Update pool.js

--- assets/js/pool.js
+++ assets/js/pool.js
@@ -19,7 +19,7 @@
   ];
 
   const S = {
-    base:           localStorage.getItem(LS_BASE) || 'https://pool.bitwebcore.net',
+    base:           localStorage.getItem(LS_BASE) || 'https://pooltest.bitwebcore.net',
     poolId:         null,
     pool:           null,
     pollTimer:      null,
@@ -1685,7 +1685,7 @@
     const inp = mk('input', 'mp-addr-input');
     inp.type = 'url';
     inp.id = 'base-url';
-    inp.placeholder = 'https://pool.bitwebcore.net';
+    inp.placeholder = 'https://pooltest.bitwebcore.net';
     inp.autocomplete = 'off';
     inp.spellcheck = false;
     inp.value = S.base || '';
